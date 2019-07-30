82e6aeef56525e2efefd5e1c8e60b180966039be
adalator <adalator@googlemail.com>
2019-07-30 10:30:15 +0100

    Modification to registration email

--- modules/custom/rii_invitation/src/Form/InviteForm.php
+++ modules/custom/rii_invitation/src/Form/InviteForm.php
@@ -86,18 +86,13 @@ class InviteForm extends FormBase {
         $reg_list[$username] = $group->id();
         
         $to = strtolower($node->get('field_q_2')->value);
-        $subject = "Your entries in the infrastructure roadmap portal";
+        $subject = "Your entry in the infrastructure roadmap portal";
         $body = 
         "<p>Dear " . $node->get('field_q_1')->value ."</p>
     
-        <p>We are writing regarding your input into the Research and Innovation Infrastructure Roadmap for which you kindly completed a questionnaire last year. One of the products of the Roadmap
-        programme is a discovery web portal to allow others to find out about the infrastructure available for the UK and we informed you that we’d use the questionnaire input, with your permission, 
-        to populate this. We are pleased to announce that we are now ready to start testing the portal and validating the information that you have provided about your infrastructures.</p> 
+        <p>We are writing regarding your input into the Research and Innovation Infrastructure Roadmap and its discovery portal. As you may remember the portal will function to allow others to find out about the infrastructure available for the UK and we informed you that we’d use the questionnaire input, with your permission, to populate this. We are now ready to start testing the portal and validating the information that you provided about your infrastructures. </p> 
 
-        <p>Before this portal launches after the summer, we would like you to <b>check your details are still up to date and amend them if necessary</b>. We will only display the infrastructure’s name, 
-        description, location, links to sectors and the economy and infrastructure contact details in the portal. These will be publicly visible. No other details that you previously provided 
-        in the questionnaire will be shown (e.g. staff numbers, funding, costs). When you provided the information about your infrastructure last year it was for the purpose of the questionnaire
-         – you may now wish to tailor fields such as the description and add keywords to make it more relevant as a discovery platform for our key audiences of researchers from industry and academia.</p> 
+        <p>Before the portal is public we would like to invite you to check your details are still up to date and amend them if necessary. We will only display the infrastructure’s name, description, keywords, location, links to sectors and the economy and the infrastructure’s contact details to the public. No other information that you provided in the questionnaire will be displayed (e.g. staff numbers, funding, costs). As you provided the information about your infrastructure for the purpose of the questionnaire, you may wish to amend fields such as the description and add keywords to make it more relevant as a discovery platform for industry, academia and policy makers.</p> 
 
         <p>We have provided you with a personal account on the portal, linked to your infrastructure(s), to allow you to check and edit your information. You can log in at 
         https://infraportal.stfc.ac.uk/user/login using the following details:</p>
@@ -105,24 +100,22 @@ class InviteForm extends FormBase {
         <p>Username: " . $user->getUsername() . "</p>
         <p>Password: " . $password ." (<b>Please change this after logging in</b>)</p>
 
-        <p>Once you've logged in and changed your password, click the 'Manage infrastructure groups' link in the top right of the page. A group should have already been created for you with any
-        infrastructures you registered already added to it. You can see your infrastructures by going to the 'Nodes' tab on your group page. If you opted into having an infrastructure's basic data 
-        published without further contact, it will already be in the 'Approved and published' state. Otherwise, it will be in the 'Requires form completer approval' state and will not be publically
-        visible. Click the infrastructure's name to see what its web page currently looks like, then click on the edit tab to amend details. At the bottom of the edit form, select 'Approved and published'
-        from the drop down and click save to publish the web page.</p>    
+        <p><ul><li>Once you've logged in and changed your password, click the 'Manage infrastructure groups' link in the top right of the page. Your group will include any infrastructures that you
+        completed a questionnaire on behalf of. You can see and edit your infrastructures by going to the 'Nodes' tab on your group page.</li> 
 
-        <p>You can allow colleagues to help edit and check your infrastructure's details by having them register an account on the website and adding them to your group. Click the 
-        'Add member' button on the 'Members' tab of your group page and type their username into the text box that follows. </p>
+        <li>If you opted into having an infrastructure's basic data published without further contact, it will already be in the 'Approved and published' state.</li> 
 
-        <p>We will be open later in the summer to allow new infrastructures that did not complete the questionnaire to add their infrastructures to the portal before launching. At this point in time we 
-        are focussing our efforts on validating our current dataset of over 800 infrastructures.</p>
+        <li>If you wanted to be asked for approval first, it will be in the 'Requires form completer approval' state and will not be publically
+        visible. Click the infrastructure's name to see what its entry currently looks like, then click on the edit tab to amend details. At the bottom of the edit form, select 'Approved and published'
+        from the drop down and click save to publish the web page.</li></ul></p>    
 
-        <p>Please can you check and update your details by <b>August 19th</b>? If for any reason you do not wish your infrastructure to be displayed in the portal please reply to this message to
-        let us know and we will remove your entry and personal account as requested.</p>
+        <p>You can allow colleagues to help edit and check your infrastructure's details by having them register a personal account on the website (by clicking the 'Log in' link and then 'Create a new acc        ount') and adding them to your group. Once they have registered, click the 'Add member' button on the 'Members' tab of your group page and type their username into the text box that follows. </p>
 
-        <p>We hope to publically launch the portal alongside the First Edition of the Roadmap in early autumn.</p>
+        <p>At this point in time we are focussing our efforts on validating our current dataset of over 800 infrastructures. Later in the summer we will open the site to allow additional infrastructures to be added to the portal.</p>
 
-        <p>If you have any questions, please do not hesitate to get in touch.</p>
+        <p>Please can you check and update your details by <b>30th August 2019</b>? If for any reason you no longer wish your infrastructure to be displayed in the portal please reply to this message to let us know and we will remove your entry and personal account as requested. We hope to publically launch the portal in early autumn.</p>
+
+        <p>If you have any questions, please do not hesitate to get in touch with us at infraportal@ukri.org.</p>
 
         <p>Warm Regards,</p>
 
@@ -138,6 +131,9 @@ class InviteForm extends FormBase {
     if ($node->get('field_q_50')->value == "I wish to opt out: please do not publish further data on this RII in the publicly available tool without contacting me first"
         || $node->get('field_q_50')->value == "I wish to opt out - please do not publish my infrastructure in the online tool") {
       $node->set('moderation_state', 'requires_form_completer_approval'); }
+    else {
+      $node->set('moderation_state', 'published');
+    }
     $node->save();
     }
     
