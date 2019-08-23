749ef36812a90051627aef499ee10bd981b12c22
adalator <adalator@googlemail.com>
2019-08-23 09:56:40 +0100

    Changed mass mailer to email everyone who hasn't updated their infra yet

--- modules/custom/rii_mass_mail/src/Form/MailQueueForm.php
+++ modules/custom/rii_mass_mail/src/Form/MailQueueForm.php
@@ -29,7 +29,7 @@ class MailQueueForm extends FormBase {
     $form['body'] = array(
       '#type' => 'textarea',
       '#title' => t('Enter email body text'),
-      '#description' => t('Enter the body text of the email you wish to send. This will be queued to be sent to all users who have not logged in yet.'),
+      '#description' => t('Enter the body text of the email you wish to send. This will be queued to be sent to owners of all infrastructures that have not been updated since I added them.'),
       '#required' => TRUE,
     );
 
@@ -44,9 +44,11 @@ class MailQueueForm extends FormBase {
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $users = user_load_multiple();
-    foreach ($users as $user) {
-      if ($user->getLastAccessedTime() == 0) {
+    $nids = \Drupal::entityQuery('node')->condition('type', 'rii')->execute();
+    $nodes = \Drupal\node\Entity\Node::loadMultiple($nids);
+    foreach ($nodes as $node) {
+      if ($node->getRevisionCreationTime() == 1563793157) {
+        $user = $node->getOwner();
         simple_mail_queue($from = NULL, $to = $user->getEmail(), $subject = $form_state->getValue('subject'), $body = $form_state->getValue('body'));
       }
     }
